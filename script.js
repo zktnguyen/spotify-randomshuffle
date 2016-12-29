393c7baf85132be4f2789b991caf12a291ef5675
zktnguyen <zktnguyen@ucdavis.edu>
2016-12-29 15:44:02 -0800

    mostly works, figuring out a way to deal with asynchronous

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 var app = {};
-var apiURL = "https://www.spotify.com/v1";
+var apiURL = "https://api.spotify.com/v1";
 
 app.numArtists = 0;
+app.playlistSize = 0;
+app.tracklist=[];
 app.getInput = function(){
   $('form').on('submit',function(e){
     e.preventDefault();
-    var artists = $('input[type=search]').val();
-    artists = artists.split(',');
-    app.numArtists = artists.length;
-    console.log(artists, app.numArtists);
+    var artists = $('input[id=searchForArtists]').val().split(',');
+    app.playlistSize = $('input[id=maxSize]').val();
+    app.getInfo(artists);
   });
 };
 
-// search for the artists
-app.searchForArtistID = function(artist){
-  var results = null;
+app.getInfo = function(artists){
+  artists.map(app.getID);
+};
+
+app.getID = function(artist){
   $.ajax({
     url: apiURL + "/search/",
     method: "GET",
@@ -24,21 +27,49 @@ app.searchForArtistID = function(artist){
       q: artist
     },
     success: function(data){
-      results = data;
+      app.searchAlbum(data.artists.items[0].id);
     }
   });
-  return results;
+
 };
 
-app.searchAlbums = function(){
+// search for the albums
 
+app.searchAlbum = function(artist){
+  $.ajax({
+    url: apiURL + "/artists/" + artist + "/albums" ,
+    method: "GET",
+    dataType: "json",
+    data: {
+      album_type: 'album'
+    },
+    success: function(data){
+      app.retrieveTracks(data);
+    }
+  });
 };
 
 // search for tracks by the artist
-app.retrieveTracks = function(){
+app.retrieveTracks = function(albums){
+  albums.items.forEach(function(album){
+    $.ajax({
+      url: apiURL + '/albums/' + album.id + '/tracks',
+      method: 'GET',
+      dataType: "json",
+      success: function(data){
+        app.createTrackList(data.items);
+      }
+    });
+  });
 
 };
 
+app.createTrackList = function(data){
+  data.forEach(function(song){
+    app.tracklist.push(song.id);
+  });
+  //console.log(app.tracklist.length);
+};
 // random tracks, split the amount of numArtists
 // create the playlist
 app.create = function() {
